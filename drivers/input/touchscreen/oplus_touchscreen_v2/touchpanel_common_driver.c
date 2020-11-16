434e2c46f39619de72cf0156f0ce243e6f5d3fb8
LibXZR <xzr467706992@163.com>
2020-11-16 21:16:51 +0800

    oplus_touchpanel_v2: Fix game mode not restoring on wakeup
    * Game mode is disabled during suspend but not restored on wakeup.
    * This could cause game mode not taking effect even when "game_switch"
      is enabled.
    
    Change-Id: I7be4c7b8ebe4664fa2af5e53178803c6045da33e

--- drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_common_driver.c
+++ drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_common_driver.c
@@ -212,6 +212,9 @@ void operate_mode_switch(struct touchpanel_data *ts)
 		if (ts->game_switch_support)
 			ts->ts_ops->mode_switch(ts->chip_data, MODE_GAME, ts->noise_level);
 
+		if (ts->game_switch_support)
+			ts->ts_ops->mode_switch(ts->chip_data, MODE_GAME, ts->noise_level);
+
 		if (ts->charger_pump_support) {
 			ts->ts_ops->mode_switch(ts->chip_data, MODE_CHARGE, ts->is_usb_checked);
 		}
