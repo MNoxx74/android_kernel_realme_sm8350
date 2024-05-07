pjgowtham/android_kernel_oplus_sm8350

sde_kms: Avoid delaying before plane cleanup or vync in recovery mode
* Causes blank screen on Q3S/9 SE

techpack/display/msm/sde/sde_kms.c
@@ -72,6 +72,8 @@
 #include "oplus_adfr.h"
 #endif
 
+#include <soc/oppo/boot_mode.h>
+
 /* defines for secure channel call */
 #define MEM_PROTECT_SD_CTRL_SWITCH 0x18
 #define MDP_DEVICE_ID            0x1A
@@ -1601,7 +1603,7 @@ static void sde_kms_wait_for_commit_done(struct msm_kms *kms,
 		 */
 		SDE_EVT32_VERBOSE(DRMID(crtc));
 		ret = sde_encoder_wait_for_event(encoder, MSM_ENC_COMMIT_DONE);
-		if (ret && ret != -EWOULDBLOCK) {
+		if ((MSM_BOOT_MODE__RECOVERY != get_boot_mode()) && (ret && ret != -EWOULDBLOCK)) {
 			SDE_ERROR("wait for commit done returned %d\n", ret);
 			sde_crtc_request_frame_reset(crtc, encoder);
 			break;

MNoxx74/android_kernel_realme_sm8350

techpack/display/msm/sde/sde_kms.c
@@ -71,6 +71,7 @@
 #endif
 
 #ifdef OPLUS_BUG_STABILITY
+#include <soc/oplus/boot_mode.h>
 #include "oplus_adfr.h"
 #endif
 
@@ -1634,7 +1635,7 @@ static void sde_kms_wait_for_commit_done(struct msm_kms *kms,
 		 */
 		SDE_EVT32_VERBOSE(DRMID(crtc));
 		ret = sde_encoder_wait_for_event(encoder, MSM_ENC_COMMIT_DONE);
-		if (ret && ret != -EWOULDBLOCK) {
+		if ((MSM_BOOT_MODE__RECOVERY != get_boot_mode()) && (ret && ret != -EWOULDBLOCK)) {
 			SDE_ERROR("wait for commit done returned %d\n", ret);
 			sde_crtc_request_frame_reset(crtc, encoder);
 			break;